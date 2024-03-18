cf57d0c11af2eaaf913b675c0a8ef26dfef27944
geno <yvesgeno@outlook.com>
2024-03-18 22:21:21 +0200

    django setup

--- busticket/busticket/settings.py
+++ busticket/busticket/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4vaa48@(&)iesp^ynaz_o-6y95j^qpvadp7*g1pnfl56$)5_4('
+SECRET_KEY = 'django-insecure--g=72r^=j$j%o%=bx2d9l55h7@p86%8pam__4xs0ai)b3jl((('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
