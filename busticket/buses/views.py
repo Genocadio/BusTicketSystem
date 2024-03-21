935b750658c284436db809f2b0d4679c9301e905
geno <yvesgeno@outlook.com>
2024-03-22 00:29:34 +0200

    added permisions and authorizations to Buses routes

--- busticket/buses/views.py
+++ busticket/buses/views.py
@@ -3,14 +3,22 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Route, Bus, Place
 from .serializers import RouteSerializer, BusSerializer, PlaceSerializer
+from rest_framework.permissions import IsAuthenticated
+from users.permisions import IsAdminUser, IsNormalUser
 
 class RouteList(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request):
         routes = Route.objects.all()
         serializer = RouteSerializer(routes, many=True)
         return Response(serializer.data)
 
     def post(self, request):
+        self.check_permissions(request)
+        permission_classes = [IsAdminUser]
+        for permission in permission_classes:
+            if not permission().has_permission(request, self):
+                return Response(status=status.HTTP_403_FORBIDDEN)
         serializer = RouteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -18,12 +26,18 @@ class RouteList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class BusList(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request):
         buses = Bus.objects.all()
         serializer = BusSerializer(buses, many=True)
         return Response(serializer.data)
 
     def post(self, request):
+        self.check_permissions(request)
+        permission_classes = [IsAdminUser]
+        for permission in permission_classes:
+            if not permission().has_permission(request, self):
+                return Response(status=status.HTTP_403_FORBIDDEN)
         serializer = BusSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
