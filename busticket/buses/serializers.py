9ef1ff2605fb2b4b29601e9ef594a9a348729092
geno <yvesgeno@outlook.com>
2024-03-22 22:13:58 +0200

    refined buses routes and trips and added authorisation as well as authentication

--- busticket/buses/serializers.py
+++ busticket/buses/serializers.py
@@ -1,17 +1,63 @@
 from rest_framework import serializers
 from .models import Route, Bus, Trip
+from django.db import models
+
+
 
 class RouteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Route
         fields = '__all__'
 
+    def validate_name(self, value):
+        if self.instance and self.instance.name == value:
+            return value
+        if Route.objects.filter(name__iexact=value).exists():
+            raise serializers.ValidationError("Route with this name already exists.")
+        return value
+
 class BusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bus
         fields = '__all__'
 
+    def validate_base_name(self, value):
+        if self.instance and self.instance.base_name == value:
+            return value
+        if Bus.objects.filter(base_name__iexact=value).exists():
+            raise serializers.ValidationError("Bus with this name already exists.")
+        return value
+
 class TripSerializer(serializers.ModelSerializer):
+    remaining_seats = serializers.SerializerMethodField()
+
     class Meta:
         model = Trip
         fields = '__all__'
+
+    def get_remaining_seats(self, obj):
+        booked_seats = Trip.objects.filter(bus=obj.bus).count()
+        remaining_seats = obj.bus.number_of_seats - booked_seats
+        return remaining_seats
+
+
+    def validate(self, data):
+        if self.instance:
+            # Check if the bus is already assigned to another trip
+            existing_trip = Trip.objects.exclude(pk=self.instance.pk).filter(bus=data['bus']).filter(
+                models.Q(departure_time__range=(data['departure_time'], data['arrival_time'])) |
+                models.Q(arrival_time__range=(data['departure_time'], data['arrival_time'])) |
+                models.Q(departure_time__lte=data['departure_time'], arrival_time__gte=data['arrival_time'])
+            )
+            if existing_trip.exists():
+                raise serializers.ValidationError("This bus is already assigned to another trip at the same time.")
+        else:
+            # Check if the bus is already assigned to another trip
+            existing_trip = Trip.objects.filter(bus=data['bus']).filter(
+                models.Q(departure_time__range=(data['departure_time'], data['arrival_time'])) |
+                models.Q(arrival_time__range=(data['departure_time'], data['arrival_time'])) |
+                models.Q(departure_time__lte=data['departure_time'], arrival_time__gte=data['arrival_time'])
+            )
+            if existing_trip.exists():
+                raise serializers.ValidationError("This bus is already assigned to another trip at the same time.")
+        return data
--- busticket/buses/views.py
+++ busticket/buses/views.py
@@ -3,14 +3,18 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Route, Bus, Trip
 from .serializers import RouteSerializer, BusSerializer, TripSerializer
+from users.permisions import IsAdminUser, IsNormalUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 
 class RouteDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsAdminUser, IsAuthenticated]
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
 
 
 class RouteList(APIView):
+    permission_classes = [IsAdminUser, IsAuthenticated]
     def get(self, request):
         routes = Route.objects.all()
         serializer = RouteSerializer(routes, many=True)
@@ -29,6 +33,7 @@ class RouteList(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class BusList(APIView):
+    permission_classes = [IsAdminUser, IsAuthenticated]
     def get(self, request):
         buses = Bus.objects.all()
         serializer = BusSerializer(buses, many=True)
@@ -47,12 +52,17 @@ class BusList(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class TripList(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request):
+        
         trips = Trip.objects.all()
         serializer = TripSerializer(trips, many=True)
         return Response(serializer.data)
 
     def post(self, request):
+        self.check_permissions(request)
+        self.permission_classes = [IsAdminUser]
+        self.check_permissions(request)
         serializer = TripSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -60,6 +70,9 @@ class TripList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request):
+        self.check_permissions(request)
+        self.permission_classes = [IsAdminUser]
+        self.check_permissions(request)
         trips = Trip.objects.all()
         trips.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
